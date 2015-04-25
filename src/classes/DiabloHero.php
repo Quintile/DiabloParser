69b4dc75c0d3db150d369f87807ecef0ce955e1b
Jordan Grieve <quintilex@gmail.com>
2015-04-25 10:18:20 -0400

    extracted more information

--- src/classes/DiabloHero.php
+++ src/classes/DiabloHero.php
@@ -8,7 +8,10 @@ class DiabloHero
 	public $name;
 	public $level;
 	public $hardcore;
-
+	public $gender;
+	public $href;
+	public $lastUpdated;
+	
 	public function __construct($class = null)
 	{
 		$this->class = $class;
--- src/classes/DiabloParser.php
+++ src/classes/DiabloParser.php
@@ -4,6 +4,8 @@ namespace Excessive\DiabloParser;
 
 use PHPHtmlParser\Dom;
 
+define('DIABLO_PATH', 'http://us.battle.net/d3/en/profile/');
+
 class DiabloParser
 {
 	protected $url;
@@ -32,36 +34,61 @@ class DiabloParser
 		$this->url = $url;
 	}
 
-	public function parse()
+	public function getHero()
 	{
-		$source = file_get_contents($this->url);
+		$source = @file_get_contents($this->url);
+		if(!$source)
+			return false;
+
+		$hero = new DiabloHero();
 
 		$dom = new Dom();
 		$dom->load($source);
 
-		$this->type = $dom->find('strong.d3-color-hardcore')->text ? 'hardcore' : 'softcore';
-		$this->seasonal = $dom->find('strong.d3-color-seasonal')->text ? true : false;
+		$hero->hardcore = $dom->find('strong.d3-color-hardcore')->text ? true : false;
+		$hero->seasonal = $dom->find('strong.d3-color-seasonal')->text ? true : false;
 		
-		$this->level = (int) $dom->find('h2.class a span strong')->text;
-		$this->class = trim($dom->find('h2.class a span')->text);
+		$hero->level = (int) $dom->find('h2.class a span strong')->text;
+		$hero->class = trim($dom->find('h2.class a span')->text);
 
-		$this->name = $dom->find('div.profile-sheet h2.name')->text;
+		$hero->name = $dom->find('div.profile-sheet h2.name')->text;
 
-		$this->life = (int) $this->convertLife($dom->find('.resource-life span.value')->text);
-		$this->damage = (double) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-dps-hero] span.value')->text;
-		$this->toughness = (int) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-toughness-hero] span.value')->text;
-		$this->recovery = (int) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-healing-hero] span.value')->text;
-		$this->str = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-strength-hero] span.value')->text;
-		$this->int = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-intelligence-hero] span.value')->text;
-		$this->vit = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-vitality-hero] span.value')->text;
-		$this->dex = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-dexterity-hero] span.value')->text;
+		$hero->life = (int) $this->convertLife($dom->find('.resource-life span.value')->text);
+		$hero->damage = (double) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-dps-hero] span.value')->text;
+		$hero->toughness = (int) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-toughness-hero] span.value')->text;
+		$hero->recovery = (int) $dom->find('ul.attributes-core li[data-tooltip=#tooltip-healing-hero] span.value')->text;
+		$hero->str = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-strength-hero] span.value')->text;
+		$hero->int = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-intelligence-hero] span.value')->text;
+		$hero->vit = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-vitality-hero] span.value')->text;
+		$hero->dex = $dom->find('ul.attributes-core li[data-tooltip=#tooltip-dexterity-hero] span.value')->text;
+		$hero->lastUpdated = $this->parseUpdated($dom->find('p.last-updated')->text);
+		return $hero;
+	}
 
-		return true;
+	public function getCareer()
+	{
+		$source = @file_get_contents($this->url);
+		if(!$source)
+			return array();
+
+		$dom = new Dom();
+		$dom->load($source);
+
+		$career = new DiabloCareer();
+		$career->lifetimeKills = (int)$dom->find('div.lifetime span.num-kills')->text;
+		$career->eliteKills = (int)$dom->find('div.elite span.num-kills')->text;
+		$career->paragon = (int)$this->extractParagon($dom->find('div.paragon span.num-kills')->text);
+		$career->hcParagon = (int)$dom->find('div.paragon span.num-kills span.d3-color-hardcore')->text;
+		$career->fallen = count($dom->find('div.fallen-hero'));
+		return $career;
 	}
 
 	public function getCharacters()
 	{
-		$source = file_get_contents($this->url);
+
+		$source = @file_get_contents($this->url);
+		if(!$source)
+			return array();
 
 		$dom = new Dom();
 		$dom->load($source);
@@ -75,15 +102,19 @@ class DiabloParser
 				continue;
 
 			$class = $this->extractClass($data[1]);
-	
+			$gender = $this->extractGender($data[1]);
+
 			$hero = new DiabloHero($class);
+			$hero->trimmedClass = str_replace(' ', '', $class);
 			$hero->name = $h->find('span.name')->text;
 			if(is_null($hero->name))
 				continue;
+			$hero->href = $h->find('a')->getAttribute('href');
 
 			$hero->level = (int)$h->find('span.level')->text;
-
+			$hero->gender = $gender;
 			$hero->hardcore = (count($data) == 3 && $data[2] !== "") ? true : false;
+			$hero->lastUpdated = $this->parseUpdated($dom->find('p.last-updated')->text);
 
 			$results[] = $hero;
 		}
@@ -91,14 +122,33 @@ class DiabloParser
 		return $results;
 	}
 
-	private function extractClass($string)
+	protected function extractParagon($string)
 	{
-		return ucwords(str_replace('-', ' ', substr($string, 0, strrpos($string, '-'))));
+		return substr($string, 0, strpos($string, ' '));
 	}
 
-	private function convertLife($value)
+	protected function extractClass($string)
+	{
+		return strtolower(str_replace('-', ' ', substr($string, 0, strrpos($string, '-'))));
+	}
+
+	protected function extractGender($string)
+	{
+		return substr($string, strrpos($string, '-')+1);
+	}
+
+	protected function convertLife($value)
 	{
 		$numerals = (double)substr($value, 0, -1);
 		return $numerals * 1000;
+	}
+
+	public function parseUpdated($string)
+	{
+		$start = strpos($string, 'on') + 2;
+		$stop = strpos($string, 'PDT');
+
+		return new \DateTime(trim(substr($string, $start, $stop-$start)), new \DateTimeZone('America/Los_Angeles'));
+
 	}
 }
